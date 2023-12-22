ede826e864dcd6c3848c628ea44477d24cbe8710
_qnk <113895291+qu4ntik@users.noreply.github.com>
2023-12-22 13:40:28 +0000

    testing git CLI

---
+++ main.py
@@ -0,0 +1,15 @@
+import pickle
+import json
+import data
+
+#1.Importer tous les fichiers JSON du fichier DATA et les input dans une function qui va return un DICT
+def json_to_dict(data_folder = data):
+    pass
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
