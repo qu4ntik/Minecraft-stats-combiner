202591d3d5acb80120a7f460ae68e6db2bf85d9d
_qnk <113895291+qu4ntik@users.noreply.github.com>
2024-01-02 12:09:47 +0100

    V1.2
    Fixed bugs

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os, json
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 stats_folder_path = os.path.join(BASE_DIR, "stats_folder")
 stats_combined = {}
-#final_json_path = os.path.join(BASE_DIR, "ur_combined_stats")
+player_uuid = ""
 
 def find_stats_folders(folder): #search and find all json files in /saves folder and copy it into the stats_folder
 
@@ -27,54 +27,56 @@ def find_stats_folders(folder): #search and find all json files in /saves folder
 
 def final_exec():
 
-        stats_combined["stats"]['minecraft:custom']["minecraft:time_since_death"] = 0 
-        stats_combined["stats"]['minecraft:custom']["minecraft:time_since_rest"] = 0
-        json_stats_combined = json.dumps(stats_combined)
+    stats_combined["stats"]['minecraft:custom']["minecraft:time_since_death"] = 0 
+    stats_combined["stats"]['minecraft:custom']["minecraft:time_since_rest"] = 0
+    json_stats_combined = json.dumps(stats_combined)
+    #print(f"json_stats_combined = {json_stats_combined}")
 
-        if not "-" in player_uuid: #simple check if the file name contain the user UUID
-            with open(os.path.join(BASE_DIR, "ur_combined_stats.json"), "w") as f:
-                f.write(json_stats_combined)
-                print(f"ur_combined_stats.json has been created, you will have to rename it with your proper UUID.")
-        
-        json_check = os.listdir(BASE_DIR) #check if there are already a .json generated at the base dir
-
-        count_json = 0 #for loop for counting the number of possible .json files in base_dir. | see (1) | note: json_check.count(".json") doesn't worked
-        for file_name in json_check:
-            if ".json" in file_name:
+    json_check = os.listdir(BASE_DIR) #check if there are already a .json generated at the base dir
+    count_json = 0 #for loop for counting the number of possible .json files in base_dir. | see (1) | note: json_check.count(".json") doesn't worked
+    for file_name in json_check:
+        if ".json" in file_name:
+            if "-" in file_name:
                 count_json += 1
 
-        duplicate_player_uuid_file_name = ""
-        print(f"count_json = {count_json}")
-        print(f"json_check = {json_check}")
+    if not "-" in player_uuid: #if the script didn't got the player UUID:
+        filename = "ur_combined_stats"
+        if count_json == 0: #if there are already no jsons in the main dir
+            with open(os.path.join(BASE_DIR, (filename + ".json")), "w") as f:
+                f.write(json_stats_combined)
+                print(f"\nur_combined_stats.json has been created, you will have to rename it with your proper UUID.")
+        else: #if jsons are already here
+            with open(os.path.join(BASE_DIR, (filename + "_" + str(count_json) + ".json")), "w") as f:
+                f.write(json_stats_combined)
+                print(f"\nYour additional .json has been created, you will have to rename it with your proper UUID.")
+    
+    else: #if the script sucefully got the player UUID:
+        player_uuid_cleared = player_uuid.split(".") #remove .json from player_uuid
+        filename = player_uuid_cleared[0]
 
-        for json_type in json_check: # (1)
-            if ".json" in json_type:
-                duplicate_player_uuid_file_name = player_uuid + str(count_json)
-                with open(os.path.join(BASE_DIR, duplicate_player_uuid_file_name), "w") as f:
-                    json_check = os.listdir(BASE_DIR)
-                    f.write(json_stats_combined)
-                    print(f"\n{duplicate_player_uuid_file_name} has been written and the file name should be your minecraft UUID.")
-                break
+        if count_json == 0: #if there are already no jsons in the main dir
+            with open(os.path.join(BASE_DIR, (filename + ".json")), "w") as f:
+                f.write(json_stats_combined)
+                print(f"\n{filename}.json has been created.")
 
-            else:     
-                with open(os.path.join(BASE_DIR, player_uuid), "w") as f:
-                    json_check = os.listdir(BASE_DIR)
-                    f.write(json_stats_combined)
-                    print(f"\n{player_uuid} has been written and the file name should be your minecraft UUID.")
+        else: #if jsons are already here
+            with open(os.path.join(BASE_DIR, (filename + "_" + str(count_json) + ".json")), "w") as f:
+                f.write(json_stats_combined)
+                print(f"\nYour additional .json has been created.")
 
-        print("\n### The script execution is completed ! ###\nYou can now find a combined stats .json in the same folder as this script.\nYou can use it to fully check your stats or to play with.\n")
+    print("\n### The script execution is completed ! ###\nYou can now find a combined stats .json in the same folder as this script.\nYou can use it to fully check your stats or to play with.\n")
 
-        try:
-            print(f"You played {round((stats_combined['stats']['minecraft:custom']['minecraft:play_time'] / 600), 1)} hours.")
-            print(f"You sneaked {round((stats_combined['stats']['minecraft:custom']['minecraft:sneak_time'] / stats_combined['stats']['minecraft:custom']['minecraft:play_time']) * 100, 2)}% of your total playtime.")
-            print(f"You jumped {stats_combined['stats']['minecraft:custom']['minecraft:jump']} times.")
-            print(f"You dealt {stats_combined['stats']['minecraft:custom']['minecraft:damage_dealt']} damage while you took {stats_combined['stats']['minecraft:custom']['minecraft:damage_taken']}.")
-            print(f"You killed {stats_combined['stats']['minecraft:custom']['minecraft:mob_kills']} mobs.")
-            print(f"You opened {stats_combined['stats']['minecraft:custom']['minecraft:open_chest']} chests.")
-            print(f"You walked {round((stats_combined['stats']['minecraft:custom']['minecraft:walk_one_cm'] / 100))} meters while you sprinted {round((stats_combined['stats']['minecraft:custom']['minecraft:sprint_one_cm'] / 100))} meters.")
-            print(f"You flew {round((stats_combined['stats']['minecraft:custom']['minecraft:fly_one_cm'] / 100))} meters and swimmed {round((stats_combined['stats']['minecraft:custom']['minecraft:swim_one_cm'] / 100))} meters.")
-        except Exception:
-            pass
+    try:
+        print(f"You played {round((stats_combined['stats']['minecraft:custom']['minecraft:play_time'] / 600), 1)} hours.")
+        print(f"You sneaked {round((stats_combined['stats']['minecraft:custom']['minecraft:sneak_time'] / stats_combined['stats']['minecraft:custom']['minecraft:play_time']) * 100, 2)}% of your total playtime.")
+        print(f"You jumped {stats_combined['stats']['minecraft:custom']['minecraft:jump']} times.")
+        print(f"You dealt {stats_combined['stats']['minecraft:custom']['minecraft:damage_dealt']} damage while you took {stats_combined['stats']['minecraft:custom']['minecraft:damage_taken']}.")
+        print(f"You killed {stats_combined['stats']['minecraft:custom']['minecraft:mob_kills']} mobs.")
+        print(f"You opened {stats_combined['stats']['minecraft:custom']['minecraft:open_chest']} chests.")
+        print(f"You walked {round((stats_combined['stats']['minecraft:custom']['minecraft:walk_one_cm'] / 100))} meters while you sprinted {round((stats_combined['stats']['minecraft:custom']['minecraft:sprint_one_cm'] / 100))} meters.")
+        print(f"You flew {round((stats_combined['stats']['minecraft:custom']['minecraft:fly_one_cm'] / 100))} meters and swimmed {round((stats_combined['stats']['minecraft:custom']['minecraft:swim_one_cm'] / 100))} meters.\n")
+    except Exception:
+        pass
 
 def combine_stats(folder):
 
@@ -101,30 +103,31 @@ def combine_stats(folder):
             except Exception:
                 continue
 
+            if "-" in file: #trying to catch the player UUID
+                player_uuid = file
+
             if stats_combined == {}:
                 print(f"{file} was defined as the reference dict.")
-                player_uuid = file
                 stats_combined = json_buffer
                 continue
 
             for category in json_buffer["stats"]:
-                #print(f"category type = {type(category)} | category = {category}")
+                print(f"category type = {type(category)} | category = {category}")
 
                 for stat in json_buffer["stats"][category]:
-                    #print(f"stat type = {type(stat)} | stat = {stat}")
-                    #fix temporaire /!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\/!\
-                    try:
-                        if stats_combined["stats"][category].get(stat) != None: #si il y a match entre les deux dicts
-                            #print(f"The stat : {stat} is getting combined...")
-                            stats_combined["stats"][category][stat] += json_buffer["stats"][category][stat]
-                    except Exception:
-                        pass
+                    print(f"stat type = {type(stat)} | stat = {stat}")
+
+                    if stats_combined["stats"][category].get(stat) != None: #si il y a match entre les deux dicts
+ 
+                        stats_combined["stats"][category][stat] += json_buffer["stats"][category][stat]
+                        print(f"The stat : {stat} is getting combined...")
     
                     else:
                         stats_combined["stats"][category][stat] = json_buffer["stats"][category][stat]
+
             print(f"{file} was effectively combined.")
 
-print("##### Welcome to my Minecraft stats combiner. #####\nYou can use ALT+C at any time to stop the script.\n")
+print("##### Welcome to my Minecraft stats combiner. #####\nYou can use CTRL+C at any time to stop the script.\n")
 print("How are you importing the stats files ?")
 main_input = input("Please enter 1 if you put them in the stats_folder, enter 2 if you want to import your minecraft folder.\n>")
 
