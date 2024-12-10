ade26d31e98826e1366a690571402970e959f3b0
_qnk <113895291+qu4ntik@users.noreply.github.com>
2024-12-10 22:17:20 +0100

    V1.3

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os, json
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 stats_folder_path = os.path.join(BASE_DIR, "stats_folder")
+#stats_folder_path_txt = os.path.join(BASE_DIR, "stats_folder\put your stats files here.txt")
 stats_combined = {}
 player_uuid = ""
 
@@ -57,26 +58,54 @@ def final_exec():
         if count_json == 0: #if there are already no jsons in the main dir
             with open(os.path.join(BASE_DIR, (filename + ".json")), "w") as f:
                 f.write(json_stats_combined)
-                print(f"\n{filename}.json has been created.")
+                print(f"{filename}.json has been created.")
 
         else: #if jsons are already here
             with open(os.path.join(BASE_DIR, (filename + "_" + str(count_json) + ".json")), "w") as f:
                 f.write(json_stats_combined)
-                print(f"\nYour additional .json has been created.")
+                print(f"Your additional .json has been created.")
 
     print("\n### The script execution is completed ! ###\nYou can now find a combined stats .json in the same folder as this script.\nYou can use it to fully check your stats or to play with.\n")
 
     try:
         print(f"You played {round((stats_combined['stats']['minecraft:custom']['minecraft:play_time'] / 600), 1)} hours.")
+    except Exception:
+        pass
+    try:
         print(f"You sneaked {round((stats_combined['stats']['minecraft:custom']['minecraft:sneak_time'] / stats_combined['stats']['minecraft:custom']['minecraft:play_time']) * 100, 2)}% of your total playtime.")
+    except Exception:
+        pass
+    try:
         print(f"You jumped {stats_combined['stats']['minecraft:custom']['minecraft:jump']} times.")
+    except Exception:
+        pass
+    try:
         print(f"You dealt {stats_combined['stats']['minecraft:custom']['minecraft:damage_dealt']} damage while you took {stats_combined['stats']['minecraft:custom']['minecraft:damage_taken']}.")
+    except Exception:
+        pass
+    try:
         print(f"You killed {stats_combined['stats']['minecraft:custom']['minecraft:mob_kills']} mobs.")
+    except Exception:
+        pass
+    try:
         print(f"You opened {stats_combined['stats']['minecraft:custom']['minecraft:open_chest']} chests.")
+    except Exception:
+        pass
+    try:
         print(f"You walked {round((stats_combined['stats']['minecraft:custom']['minecraft:walk_one_cm'] / 100))} meters while you sprinted {round((stats_combined['stats']['minecraft:custom']['minecraft:sprint_one_cm'] / 100))} meters.")
+    except Exception:
+        pass
+    try:
         print(f"You flew {round((stats_combined['stats']['minecraft:custom']['minecraft:fly_one_cm'] / 100))} meters and swimmed {round((stats_combined['stats']['minecraft:custom']['minecraft:swim_one_cm'] / 100))} meters.\n")
     except Exception:
         pass
+    try:
+        print(f"You dropped {stats_combined['stats']['minecraft:custom']['minecraft:dropped']} items.")
+    except Exception:
+        pass
+
+    print("\nThanks u for using the minecraft stats combiner !")
+    input("Press Enter to quit the program\n>")
 
 def combine_stats(folder):
 
@@ -117,19 +146,24 @@ def combine_stats(folder):
                 for stat in json_buffer["stats"][category]:
                     print(f"stat type = {type(stat)} | stat = {stat}")
 
-                    if stats_combined["stats"][category].get(stat) != None: #si il y a match entre les deux dicts
- 
-                        stats_combined["stats"][category][stat] += json_buffer["stats"][category][stat]
-                        print(f"The stat : {stat} is getting combined...")
+                    try:
+                        if stats_combined["stats"][category].get(stat) != None: #si il y a match entre les deux dicts
     
-                    else:
-                        stats_combined["stats"][category][stat] = json_buffer["stats"][category][stat]
+                            stats_combined["stats"][category][stat] += json_buffer["stats"][category][stat]
+                            print(f"The stat : {stat} is getting combined...")
+        
+                        #shit fix please help me to run this w/o try except ############################
+                        else:
+                            stats_combined["stats"][category][stat] = json_buffer["stats"][category][stat]
+                    except Exception:
+                        pass
+
 
             print(f"{file} was effectively combined.")
 
 print("##### Welcome to my Minecraft stats combiner. #####\nYou can use CTRL+C at any time to stop the script.\n")
 print("How are you importing the stats files ?")
-main_input = input("Please enter 1 if you put them in the stats_folder, enter 2 if you want to import your minecraft folder.\n>")
+main_input = input("Enter 1 if you put the statistics files in the stats_folder\nEnter 2 if you want to import your minecraft folder.\n>")
 
 while main_input not in "12":
     print("Please enter a valid number.")
@@ -138,17 +172,18 @@ while main_input not in "12":
 
 if main_input == "1":
 
-    if os.listdir(stats_folder_path) == []:
+    while os.listdir(stats_folder_path) == []:
         print("\nYour stats_folder is empty.")
-
+        input("Please put some statistics files in the stats_folder\n>")
+ 
     combine_stats(stats_folder_path)
 
 if main_input == "2":
 
-    minecraft_map_folder_path = input("\nPlease enter your minecraft or curseforge folder path.\nYou have to right click to use copy/paste here.\n>")
+    minecraft_map_folder_path = input("\nPlease enter your minecraft or curseforge folder path.\nYou could have to right click to use copy/paste here.\n>")
 
     while os.path.exists(minecraft_map_folder_path) == False:
-        minecraft_map_folder_path = input("Your path is incorrect.\n>")
+        minecraft_map_folder_path = input("Please enter a correct and complete path.\n>")
 
     find_stats_folders(minecraft_map_folder_path)
 
